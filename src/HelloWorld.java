8bf59ca037063fafa8e29f42d6b02832f9f79959
htnha <hoang.tuan.nha@gmail.com>
2023-01-09 08:32:38 +0700

    Tao file HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[]args){
+        System.out.println("Chao K63");
+        for(int i = 0;i < 10; i++){
+            System.out.println(i);
+        }
+    }
+}

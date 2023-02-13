a47b08201fbb234730ab13f391b7189397613d62
htnha <hoang.tuan.nha@gmail.com>
2023-02-13 08:32:33 +0700

    Ket noi Database

--- src/person/TestPerson.java
+++ src/person/TestPerson.java
@@ -1,14 +1,53 @@
 package person;
 
+import java.sql.*;
+
 public class TestPerson {
-    public static void main(String[]args){
-        Person student1 = new Student("Cuong", 22, "Ba Dong", 4);
-        student1.setName("ABC");
-        Person teacher1 = new Teacher("Nha", 38, "Dong Hoi", 5000000);
-        Person[] list = {student1, teacher1};
-        for (Person person: list
-             ) {
-            person.display();
+    static final String DB_URL = "jdbc:mysql://localhost/TEST";
+    static final String USER = "root";
+    static final String PASS = "";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT id, name, salary FROM Employees";
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                int id  = rs.getInt("id");
+                String name = rs.getString("name");
+                int salary = rs.getInt("salary");
+                System.out.print("ID: " + id);
+                System.out.print(", Name: " + name);
+                System.out.println(", Salary: " + salary);
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            se.printStackTrace();
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if(stmt!=null)
+                    stmt.close();
+            }catch(SQLException se2){
+            }
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
         }
+        System.out.println("Goodbye!");
     }
+
 }
